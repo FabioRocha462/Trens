0e3b733f1b6394344c98a5053bf620e5cbe6a1f0
Fábio Rocha <rochafabio462@gmail.com>
2023-01-17 09:01:50 -0300

    comentários

--- Trem.py
+++ Trem.py
@@ -1,10 +1,15 @@
+#bibliotecas utilizadas
 import time as t
 import threading as th
 from tkinter import *
 from PIL import ImageTk, Image
 
+#semáfors dos trens
 
+#variável global para o start e o stop dos trens
 START_TREM = True
+
+#funções para o start e o stop
 def stop():
     global START_TREM
     START_TREM = False
@@ -12,16 +17,18 @@ def start():
     global START_TREM
     START_TREM = True
 
+
+#class trem
 class Trem:
-    def __init__(self,coordenadaXI,coordenadaYI,cooderdanaXF,coordernadaYF, canvas,timing,trem):
-        self.coordenadaXI = coordenadaXI
-        self.coordenadaYI = coordenadaYI
-        self.cooderdanaXF = cooderdanaXF
-        self.cooderdanaYF = coordernadaYF
-        self.canvas = canvas
-        self.timing = timing
-        self.trem = trem
+    #construtor inicial
+    def __init__(self,coordenadaXI,coordenadaYI, canvas,timing,trem):
+        self.coordenadaXI = coordenadaXI #x-inicial que corresponse o sentido horizontal
+        self.coordenadaYI = coordenadaYI #y-inicial que corresponse o sentido vertical
+        self.canvas = canvas #canvas para manipular as imagens e atualizar posições
+        self.timing = timing #timing para controlar o tempo de cada trem irá movimentar-se
+        self.trem = trem #imagem do trem criada com o canvas do app
         
+        #colocando a imagem do trem na posição passadas como argumento
         TremEnv = self.canvas.create_image(
             self.coordenadaXI,
             self.coordenadaYI,
@@ -29,16 +36,21 @@ class Trem:
             anchor = NW
         )
         
-        self.imagem = TremEnv
-                    
-def move(Trem,canvas):
-    global START_TREM
-    coordx = float(Trem.coordenadaXI)
-    coordy = float(Trem.coordenadaYI)
-    x = 0
+        self.imagem = TremEnv #agora passamos o tremEnv para a imagem criada
+
+
+#função mover os trens            
+def move(Trem,canvas): #recebemos o trem e o canvas (o trem para termos acesso as infirmações de imagem e o canvas para pegar a posiçao geral)
+    global START_TREM # aqui tornamos o START_TREM como global
+    coordx = float(Trem.coordenadaXI) #obtemos o coordenadaX do trem
+    coordy = float(Trem.coordenadaYI) #obtemos o coordenadaY do trem
+    #posições x e y para atualizar o move
+    x = 0 
     y = 0
+
+    # fazendo o laço de repetição entrar em loop
     while (START_TREM):
-        coordenadas = canvas.coords(Trem.imagem)
+        coordenadas = canvas.coords(Trem.imagem) # obtendo as coordenadas do canvas
         if (coordenadas[0] == coordx) and (coordenadas[1] > coordy - 180):
             x = 0
             y = -5
@@ -54,14 +66,17 @@ def move(Trem,canvas):
         if (coordenadas[1] == coordy) and (coordx  < coordenadas[0]):
             y = 0
             x = -5
-
-        canvas.move(Trem.imagem,x,y)
+        
+        canvas.move(Trem.imagem,x,y)#podemos mover os trens com a função do canvas chamada move passando estes três argumentos
+        #fazemos o update
         root.update()
+        #colocamos para dormir
         t.sleep(Trem.timing)
 
             
-
+#class App
 class App(object):
+    #construtor
     def __init__(self, app, **kwargs):
         global background_image
         self.app = app
@@ -103,10 +118,10 @@ class App(object):
         trem3 = ImageTk.PhotoImage(Image.open('trem.png').resize((30,30)))
         trem4 = ImageTk.PhotoImage(Image.open('trem.png').resize((30,30)))
 
-        TremInstance1 = Trem(135,430,0,0,self.canvas,float(self.TimeTrem1.get()),trem1)
-        TremInstance2 = Trem(310,430,0,0,self.canvas,float(self.TimeTrem2.get()),trem2)
-        TremInstance3 = Trem(485,430,0,0,self.canvas,float(self.TimeTrem3.get()),trem3)
-        TremInstance4 = Trem(310,250,0,0,self.canvas,float(self.TimeTrem4.get()),trem4)   
+        TremInstance1 = Trem(135,430,self.canvas,float(self.TimeTrem1.get()),trem1)
+        TremInstance2 = Trem(310,430,self.canvas,float(self.TimeTrem2.get()),trem2)
+        TremInstance3 = Trem(485,430,self.canvas,float(self.TimeTrem3.get()),trem3)
+        TremInstance4 = Trem(310,255,self.canvas,float(self.TimeTrem4.get()),trem4)   
 
         th.Thread(target=move,args=(TremInstance1,self.canvas)).start()
         th.Thread(target=move,args=(TremInstance2,self.canvas)).start()
